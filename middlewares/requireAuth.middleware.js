e02d636b973579370d8b650c76fb76e4556d212d
varditomer <tomer.vardi@gmail.com>
2023-03-09 16:56:11 +0200

    remove import ../config from requireAuth.middleware

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,14 +1,14 @@
 const authService = require('../api/auth/auth.service')
 const logger = require('../services/logger.service')
-const config = require('../config')
+// const config = require('../config')
 const tweetService = require('../api/tweet/tweet.service')
 
 function requireAuth(req, res, next) {
 
-  if (config.isGuestMode && !req?.cookies?.loginToken) {
-    req.loggedinUser = { _id: '', fullname: 'Guest' }
-    return next()
-  }
+  // if (config.isGuestMode && !req?.cookies?.loginToken) {
+  //   req.loggedinUser = { _id: '', fullname: 'Guest' }
+  //   return next()
+  // }
 
   if (!req?.cookies?.loginToken) return res.status(401).send('Not Authenticated')
 
