149c3e21bf629748e54d70a2673bba26086360c6
varditomer <tomer.vardi@gmail.com>
2023-03-13 21:53:50 +0200

    fix show tweet & reply option only in mobile and on dektop in hover

--- dist/public/assets/cancel_small-6239ed8f.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-008ac2df.js";const s=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",height:24,viewBox:"0 96 960 960",width:24,...t},e.createElement("path",{d:"m256 856-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"})),l="/assets/cancel_small-20ed346d.svg";export{s as ReactComponent,l as default};
